19483aed706a6bacefda1592348c9bc8b7b49e6f
Kunjal Sheth <kunjal.sheth@hp.com>
2021-04-21 23:51:12 +0530

    completed frontend1

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,62 +1,74 @@
-import React,{Component} from 'react';
-import './sign-in.styles.scss';
+import React from 'react';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-class SignIn extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            email:'',
-            password:''
-        }
-    }
+import './sign-in.styles.scss';
 
-    //function to set the value whenever submit is clicked
-    handleSubmit=async event=>{
-        event.preventDefault();
-        const{email, password}= this.state;
-        try{
-            await auth.signInWithEmailAndPassword(email,password);
-            this.setState({email:'',password:''})
-        } catch(error){
-            console.log(error);
-        }
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
     };
-        
-    
+  }
 
-    //function to capture the value of email and password typed by user
-    handleChange=event=>{ //event is called whenever there is a request to handle the change
-        //event has various properties , we used target and this target has value and name
-        const {value,name}= event.target; 
-        //to dynamically set property value
-        this.setState({[name]:value}) 
-    }
+  handleSubmit = async event => {
+    event.preventDefault();
 
-    render(){
-        const {email,password}=this.state;
-        return(
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput name="email" type="email" value={email} handleChange={this.handleChange} required='true' label='Email'/>
-                    <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} required='true' label='Password'/>
-                    <div classname='buttons'>
-                        <CustomButton type="submit">Sign In</CustomButton>
-                        <CustomButton onClick={ signInWithGoogle } isGoogleSignIn>
-                            
-                            Sign in with Google
-                    </CustomButton>
-                    </div>
-                   
-                </form>
-            </div>
-        );
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            collections : SHOP_DATA
-        }
-    }
+import SHOP_DATA from './shop.data.js';
 
-    render(){
-        //destructuring collection to avoid writing collections again
-        const {collections}=this.state;
-        return(
-            <div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))
-            }
-            </div>
-        )
-    }
+import CollectionPreview from '../../components/collection-preview/collection-preview';
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ShopPage;
